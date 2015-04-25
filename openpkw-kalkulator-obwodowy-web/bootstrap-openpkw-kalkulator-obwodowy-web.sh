69e1077ee6e48eb9bfc3a73ff229f279a4d96129
Sebastian Celejewski <sebastian.celejewski@wp.pl>
2015-04-25 08:42:36 +0200

    Poprawki po przeniesieniu konfiguracji serwerów do osobnych katalogów

--- openpkw-kalkulator-obwodowy-web/bootstrap-openpkw-kalkulator-obwodowy-web.sh
+++ openpkw-kalkulator-obwodowy-web/bootstrap-openpkw-kalkulator-obwodowy-web.sh
@@ -18,14 +18,14 @@ sudo useradd openpkw-cd -m
 sudo mkdir /var/www/html/openpkw-kalkulator-obwodowy-web
 sudo chmod a+rwx /var/www/htmlopenpkw-kalkulator-obwodowy-web
 sudo mkdir /home/openpkw-cd/.ssh
-sudo cp /vagrant/openpkw-kalkulator-obwodowy-web/.ssh/authorized_keys /home/openpkw-cd/.ssh/authorized_keys
+sudo cp /vagrant/.ssh/authorized_keys /home/openpkw-cd/.ssh/authorized_keys
 sudo chown openpkw-cd:openpkw-cd /home/openpkw-cd/.ssh
 sudo chown openpkw-cd:openpkw-cd /home/openpkw-cd/.ssh/authorized_keys
 sudo chmod 700 /home/openpkw-cd/.ssh
 sudo chmod 600 /home/openpkw-cd/.ssh/authorized_keys
 
-sudo cp /vagrant/openpkw-kalkulator-obwodowy-web/etc/apache2/sites-available/openpkw-kalkulator-obwodowy-web.conf /etc/apache2/sites-available/openpkw-kalkulator-obwodowy-web.conf
-sudo cp /vagrant/openpkw-kalkulator-obwodowy-web/index.html /var/www/html/openpkw-kalkulator-obwodowy-web/index.html
+sudo cp /vagrant/etc/apache2/sites-available/openpkw-kalkulator-obwodowy-web.conf /etc/apache2/sites-available/openpkw-kalkulator-obwodowy-web.conf
+sudo cp /vagrant/index.html /var/www/html/openpkw-kalkulator-obwodowy-web/index.html
 sudo chmod a+rwx /var/www/html/openpkw-kalkulator-obwodowy-web/index.html
 
 sudo a2dissite 000-default.conf
