6e8b6a8a8f8a03267483c6418d41b7846cc148ee
Sebastian Celejewski <sebastian.celejewski@wp.pl>
2015-10-01 07:24:02 +0200

    Modyfikacja skryptu inicjującego środowisko aby nie uruchamiał JBossa, ale instalował i uruchamiał WildFly

--- openpkw-dokument-generator/bootstrap-openpkw-dokument-generator.sh
+++ openpkw-dokument-generator/bootstrap-openpkw-dokument-generator.sh
@@ -19,4 +19,19 @@ sudo /usr/share/jboss/bin/add-user.sh --silent=true jenkins $jenkins_password
 sudo cp /vagrant/jboss/standalone.xml /usr/share/jboss/standalone/configuration/standalone.xml
 sudo cp /vagrant/etc/init.d/jbossas7 /etc/init.d/jbossas7
 sudo chmod +x /etc/init.d/jbossas7
-sudo service jbossas7 start
+#sudo service jbossas7 start
+
+----
+echo [Installing WildFly]
+sudo wget http://download.jboss.org/wildfly/9.0.1.Final/wildfly-9.0.1.Final.tar.gz -P /usr/src/
+sudo tar xfvz /usr/src/wildfly-9.0.1.Final.tar.gz -C /usr/share/
+sudo ln /usr/share/wildfly-9.0.1.Final /usr/share/wildfly -s
+sudo useradd wildfly
+sudo chown -fR wildfly.wildfly /usr/share/wildfly-9.0.1.Final
+jenkins_password=`cat /vagrant/jboss/jenkins_password`
+sudo /usr/share/wildfly/bin/add-user.sh --silent=true jenkins $jenkins_password
+sudo cp /vagrant/wildfly/standalone.xml /usr/share/wildfly/standalone/configuration/standalone.xml
+sudo cp /vagrant/etc/init.d/wildfly9 /etc/init.d/wildfly9
+sudo chmod +x /etc/init.d/wildfly9
+sudo service wildfly9 start
+
